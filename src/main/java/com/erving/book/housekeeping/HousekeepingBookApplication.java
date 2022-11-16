5571901082c17c9fc6b415f08d286b7e643d2ff9
erving <erving.kim@snowcorp.com>
2022-11-16 21:02:18 +0900

    Feat: 프로젝트 초기화
    Spring Boot version 2.7.5
    Java version 1.8
    Maven

---
+++ src/main/java/com/erving/book/housekeeping/HousekeepingBookApplication.java
@@ -0,0 +1,13 @@
+package com.erving.book.housekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousekeepingBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousekeepingBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erving/book/housekeeping/HousekeepingBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erving.book.housekeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousekeepingBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
